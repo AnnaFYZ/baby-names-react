7a16f62961ff4d1811093392e145f9c0f490aada
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-06-27 23:15:46 +0100

    level 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
 import babyNameList from "./babyNamesData.json";
 import './App.css';
+import { useState } from "react";
 
-function NamesFilter () {
+
+function NamesFilter (props) {
+  const [listToDisplay, setListToDisplay] = useState(props.list)
+  const [search, setSearch] = useState("");
+  const [favorites, setFavorites] = useState([]);
+  
+  function filterFavorites (event) {
+    setFavorites(favorites.concat({"name": event.target.innerText,
+          "sex": event.target.className}))
+    setListToDisplay(listToDisplay.filter((item) => item.name !== event.target.innerText))
+  }
+
+  function removeFavorites (e) {
+    setFavorites(favorites.filter((item) => item.name !== e.target.innerText))
+    setListToDisplay(listToDisplay.concat(props.list.filter((item) => item.name === e.target.innerText)))
+  }
   return (
-    <div id="nameList">
-      {babyNameList
-        .sort((a, b) => a.name.localeCompare(b.name))
-        .map((name) => {
-          return <span className={name.sex}>{name.name}</span>;
-        })}
+    <div>
+      <input
+        type="text"
+        name="searchInput"
+        className="SearchField"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div id="favoriteList">
+        <h2>Favorites: </h2>
+        {favorites
+          .filter((item) => {
+            return search.toLowerCase() === ""
+              ? item
+              : item.name.toLowerCase().includes(search);
+          })
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .map((name) => {
+            return (
+              <span
+                key={name.name}
+                className={name.sex}
+                onClick={removeFavorites}
+              >
+                {name.name}
+              </span>
+            );
+          })}
+      </div>
+      <div id="nameList">
+        {listToDisplay
+          .filter((item) => {
+            return search.toLowerCase() === ""
+              ? item
+              : item.name.toLowerCase().includes(search);
+          })
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .map((name) => {
+            return (
+              <span
+                key={name.name}
+                className={name.sex}
+                onClick={filterFavorites}
+              >
+                {name.name}
+              </span>
+            );
+          })}
+      </div>
     </div>
   );
-
 }
 
 function App() {
   return (
     <div className="App">
-     <NamesFilter />
+    <NamesFilter list={babyNameList}/>
     </div>
   );
 }
