c0e47b088bb65165e7cfd02a793c9f8400e133a0
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-06-29 22:41:45 +0100

    buttons added but not functional yet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import babyNameList from "./babyNamesData.json";
 import './App.css';
 import { useState } from "react";
 
+function GenderButton (props) {
+  return (
+    <div>
+      <label className="Label"> 
+        <img className="genderButton" src={props.src} alt="gender" />
+        <input
+          id={props.id}
+          name="gender"
+          className="hidden"
+          type="radio"
+          value={props.value}
+          defaultChecked={props.defaultChecked}
+          // style={}
+        ></input>
+      </label>
+    </div>
+  );
+}
 
 function NamesFilter (props) {
   const [listToDisplay, setListToDisplay] = useState(props.list)
@@ -20,15 +38,45 @@ function NamesFilter (props) {
   }
   return (
     <div>
-      <input
-        type="text"
-        name="searchInput"
-        className="SearchField"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
+      <div className="SearchBar">
+        <input
+          type="text"
+          name="searchInput"
+          className="SearchField"
+          value={search}
+          placeholder="Search for a name..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <GenderButton
+          id="allGenders"
+          src="./boy-and-girl.png"
+          value=""
+          defaultChecked={true}
+          
+        />
+        <GenderButton
+          id="male"
+          src="./baby-boy.png"
+          value="m"
+          defaultChecked={false}
+          
+        />
+        <GenderButton
+          id="female"
+          src="./baby-girl1.png"
+          value="f"
+          defaultChecked={false}
+          
+        />
+      </div>
       <div id="favoriteList">
-        <h2>Favorites: </h2>
+        <h2>
+          Favorites:
+          <span style={{ color: "grey" }}>
+            {favorites.length === 0 &&
+              " ...Click some names below to add to your shortlist..."}
+          </span>
+        </h2>
         {favorites
           .filter((item) => {
             return search.toLowerCase() === ""
